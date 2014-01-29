e067553244264025c6ff340f43f54183ceac8bfc
mathieu longtin <mathieu.longtin@nuance.com>
2014-01-28 22:50:22 -0500

    added kombu/rabbitmq test

---
+++ test_kombu.py
@@ -0,0 +1,92 @@
+import logging
+import os
+import subprocess
+import time
+import utils
+import kombu, kombu.pools, kombu.common
+
+logger = logging.getLogger(__name__)
+
+class RabbitMqTester(utils.AsyncQueueTester):
+    #server_process = None
+    #server_tmp_dir = 'tmp'
+
+    #def start_server(self):
+    #    self.server_process = subprocess.Popen([
+    #        './servers/nsqd',
+    #        '-data-path=%s' % self.server_tmp_dir,
+    #        '-http-address=127.0.0.1:4151',
+    #        '-tcp-address=127.0.0.1:4150'])
+
+    #    time.sleep(2)
+
+    #def stop_server(self):
+    #    if not self.server_process: return
+    #    self.server_process.terminate()
+    #    status = self.server_process.wait()
+    #    if status != 0:
+    #        logger.warn("nsqd exited with %d", status)
+    #    for filename in os.listdir(self.server_tmp_dir):
+    #        if filename.endswith('.dat'):
+    #            path = os.path.join(self.server_tmp_dir,filename)
+    #            logger.info("Removing %s", path)
+    #            os.unlink(path)
+
+    def connect(self):
+        self.connection = kombu.Connection('amqp://')
+        self.task_exchange = kombu.Exchange('test', type='direct')
+        self.producer = kombu.Producer(self.connection, exchange=self.task_exchange)
+        kombu.common.maybe_declare(self.task_exchange, self.connection)
+        self.task_queues = [ ]
+        for queue_name in self.queues.keys():
+            queue = kombu.Queue(queue_name, self.task_exchange, routing_key=queue_name)
+            self.task_queues.append(queue)
+            kombu.common.maybe_declare(queue, self.connection)
+
+    def run_loader(self, msg_generator):
+        self.connect()
+        for queue, body in msg_generator:
+            self.send(queue,body)
+
+        start = time.time()
+        self.connection.release()
+        release_time = time.time() - start
+        if release_time > 1.0:
+            logger.info("Connection release time: %f s", release_time)
+
+    class Job(object):
+        def __init__(self,message,body):
+            self.message = message
+            self.queue,self.body = body
+
+        def done(self):
+            self.message.ack()
+
+    def run_worker(self, job_processor, timeout=2):
+        self.connect()
+
+        def message_handler(body, message):
+            self.last_recv = time.time()
+            #logger.info("Got message: %s", body)
+            job = self.Job(message,body)
+            job_processor(job)
+
+        with kombu.Consumer(self.connection, queues=self.task_queues, callbacks=[message_handler]) as consumer:
+            while True:
+                try:
+                    self.connection.drain_events(timeout=1)
+                except Exception as error:
+                    logger.error("Drain error: %s", error)
+                    break
+
+    def send(self, queue, message):
+        self.producer.publish([queue,message], 
+                routing_key=queue,
+                serializer='json')
+
+if __name__ == '__main__':
+    RabbitMqTester.main()
+
+
+
+
